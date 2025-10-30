43f89945106c45e4b9b923a025568675fb7fc57b
Jonas LEwandrowski <joecarverde@gmail.com>
2025-10-30 12:06:53 +0100

    Add blind_state_converter plugin functionality (some todos left but i dont think those are really necessary since the controller doesnt use them)

--- ros2/msgs_ws/src/message_converter/launch/message_converter.launch.py
+++ ros2/msgs_ws/src/message_converter/launch/message_converter.launch.py
@@ -3,8 +3,6 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, OpaqueFunction
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
@@ -12,27 +10,21 @@ def generate_launch_description():
     # Get the package directory
     pkg_dir = get_package_share_directory('message_converter')
 
-
-    # Conditionally select contact plugin configuration
+    # Configuration file path
     blind_state_config_path = os.path.join(
         pkg_dir, 'config', 'contact_plugin.yaml'
     )
 
-    # Function to launch the node with conditional config
-    def launch_node(context, *args, **kwargs):
-        # Create the node with selected configuration
-        node = Node(
-            package='message_converter',
-            executable='message_converter_node',
-            name='message_converter',
-            output='screen',
-            parameters=[
-                {'use_sim_time': True},
-                blind_state_config_path
-            ]
-        )
-        return [node]
+    # Create the node with configuration
+    node = Node(
+        package='message_converter',
+        executable='message_converter_node',
+        name='message_converter',
+        output='screen',
+        parameters=[
+            blind_state_config_path,
+            {'use_sim_time': True}
+        ]
+    )
     
-    return LaunchDescription([
-        OpaqueFunction(function=launch_node)
-    ])
+    return LaunchDescription([node])
--- ros2/msgs_ws/src/message_converter/src/plugins/blind_state_converter_plugin.cpp
+++ ros2/msgs_ws/src/message_converter/src/plugins/blind_state_converter_plugin.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <sensor_msgs/msg/joint_state.hpp>
 #include <state_estimator_msgs/msg/contact_detection.hpp>
+#include <dls2_msgs/msg/blind_state_msg.hpp>
 
 #include <message_filters/time_synchronizer.hpp>
 #include <message_filters/subscriber.hpp>
@@ -14,8 +15,10 @@ namespace message_converter
 {
 	using JointState = sensor_msgs::msg::JointState;
   using Contact = state_estimator_msgs::msg::ContactDetection;
+  using BlindData = dls2_msgs::msg::BlindStateMsg;
   using ApproximateTimePolicy = message_filters::sync_policies::ApproximateTime<JointState, Contact>;
 	using ExactTimePolicy = message_filters::sync_policies::ExactTime<JointState, Contact>;
+  #define MySyncPolicy ApproximateTimePolicy
   class BlindStateConverter : public message_converter::PluginBase
   {
 
@@ -37,20 +40,57 @@ namespace message_converter
           return std::string("A converter that aggregates odometry, joint states and legodometry to produce state estimation");
         }
 
+        void syncCallback(const JointState::ConstSharedPtr &joint_state, const Contact::ConstSharedPtr &contact)
+        {
+            RCLCPP_INFO(node_->get_logger(), "Synchronized JointState and ContactDetection received.");
+
+            // publishing
+			      msg_.timestamp = node_->get_clock()->now();
+			      msg_.frame_id = "base_link";
+            //joint state values
+            msg_.joints_name = joint_state->name;
+            msg_.joints_position = joint_state->position;
+            msg_.joints_velocity = joint_state->velocity;
+            msg_.joints_effort = joint_state->effort;
+            //TODO: Calculate acceleration? Set to 0 for now
+            //TODO: Only use temperature if necessary
+            //TODO see if this is even used (doesnt seem like it)
+            //msg_.feet_contact = contact->
+
+            blind_state_pub_->publish(msg_);
+
+
+
+
+        }
+
     protected:
         void initialize_() override
         {
+            auto node = this->node_;
             RCLCPP_INFO(node_->get_logger(), "Blind State Converter initialized.");
             // TODO: Add subscriptions, publishers, timers here
             // declare parameters with sensible defaults and read their values
             auto contact_topic = node_->declare_parameter<std::string>("contactdetection_topic", "/contact_detection");
             auto joint_states_topic = node_->declare_parameter<std::string>("joint_states_topic", "/joint_states");
+            auto pub_topic = node_->declare_parameter<std::string>("pub_topic", "/blind_state_topic");
+            auto sync_tolerance_ms = node_->declare_parameter<int>("sync_tolerance_ms", 200);
 
-            // log the declared parameter values
-            RCLCPP_INFO(node_->get_logger(), "contactdetection_topic = %s", contact_topic.c_str());
-            RCLCPP_INFO(node_->get_logger(), "joint_states_topic = %s", joint_states_topic.c_str());
+            //Subscribers
+            auto qos = rclcpp::SensorDataQoS();
 
+            joint_state_sub_ = std::make_shared<message_filters::Subscriber<JointState>>(node, joint_states_topic,qos);
+            contact_sub_ = std::make_shared<message_filters::Subscriber<Contact>>(node, contact_topic,qos);
 
+            //Synchronizer
+            sync_ =  std::make_shared<message_filters::Synchronizer<MySyncPolicy>>(
+                MySyncPolicy(10), *joint_state_sub_, *contact_sub_);
+            sync_->setMaxIntervalDuration(rclcpp::Duration::from_seconds(sync_tolerance_ms / 1000.0));
+            sync_->registerCallback(
+                std::bind(&BlindStateConverter::syncCallback, this, std::placeholders::_1, std::placeholders::_2));
+
+            //Publisher
+            blind_state_pub_ = node_->create_publisher<BlindData>(pub_topic, qos);
         }
 
         void shutdown_() override
@@ -75,6 +115,11 @@ namespace message_converter
 
     private:
         std::shared_ptr<message_filters::Subscriber<JointState>> joint_state_sub_;
+        std::shared_ptr<message_filters::Subscriber<Contact>> contact_sub_;
+        std::shared_ptr<message_filters::Synchronizer<ApproximateTimePolicy>> sync_;
+
+        rclcpp::Publisher<BlindData>::SharedPtr blind_state_pub_;
+        BlindData msg_;
 
   };
 }
